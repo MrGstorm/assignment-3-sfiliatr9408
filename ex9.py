2f22d0ee1b7e48d4516a369f9d954ebe6945dea7
sfiliatr9408 <sfiliatr9408@hwdsb.on.ca>
2025-10-03 08:51:44 -0400

    Add files via upload

---
+++ ex9.py
@@ -0,0 +1,11 @@
+###############################################################################
+#  Program Name   : ex9.py
+#  Author         : Samantha. F
+#  Task           : (Create a program that prints only all odd numbers from 1 to 19.)
+###############################################################################
+
+
+# Program to print odd numbers for 1 to 19.
+
+for number in range(1, 20, 2):
+    print(number)
