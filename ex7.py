09641a837f0e0e0822efe178e1442cdeea149046
sfiliatr9408 <sfiliatr9408@hwdsb.on.ca>
2025-10-03 08:51:11 -0400

    Add files via upload

---
+++ ex7.py
@@ -0,0 +1,23 @@
+###############################################################################
+#  Program Name   : ex7.py
+#  Author         : Samantha. F
+#  Task           : (Create a program that asks for two different numbers and print the bigger one.)
+###############################################################################
+
+
+
+#This is where I get the user to enter their numbers
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the second number:"))
+
+#Compare and print the bigger number
+if num1 > num2:
+    print("The bigger number is:",
+num1)
+if num2 > num1:
+    print("The bigger number is:",
+num2)
+if num1 == num2:
+    print("Both numbers are equal.")
+    
+a= input()
