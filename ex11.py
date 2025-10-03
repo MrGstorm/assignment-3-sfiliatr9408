a4c27da712c08a1a85df18a37bda477d1901e922
sfiliatr9408 <sfiliatr9408@hwdsb.on.ca>
2025-10-03 08:52:12 -0400

    Add files via upload

---
+++ ex11.py
@@ -0,0 +1,12 @@
+###############################################################################
+#  Program Name   : ex11.py
+#  Author         : Samantha. F
+#  Task           : (Create a function that uses your name and age as parameters and prints out a sentence about you.)
+###############################################################################
+
+
+def about_me(name, age):
+    return f"My name is {name} and I am {age} years old."
+# Example
+sentence = about_me("Samantha", 15)
+print(sentence)
